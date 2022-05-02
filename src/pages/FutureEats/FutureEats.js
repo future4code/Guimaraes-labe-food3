7e5a198fc400daf9099da971b7da4b8d03ea06fb
robsonribeiro <robsonribeiro@uffa.com.vc>
2022-05-02 19:45:29 -0300

    criado o component do card

---
+++ src/pages/FutureEats/FutureEats.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Card from "../../components/Card/Card"
+
+import { FutureEatsContainer } from './styles'
+
+const FutureEats = () => {
+    return(<FutureEatsContainer>
+        <Card />
+        <Card />
+        <Card />
+    </FutureEatsContainer>)
+}
+
+export default FutureEats;
