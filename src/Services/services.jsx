7f10120ab69002d31d33ee6ac7cf1e26aaf9843c
robsonribeiro <robsonribeiro@uffa.com.vc>
2022-05-06 20:15:34 -0300

    criado chamado do restaurante

--- src/Services/services.jsx
+++ src/Services/services.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import { BASE_URL } from "../constant/urls";
 
 export const address = async (payload, authToken) => {
-    await axios.pull(`${BASE_URL}/address`, payload,
-    { headers: {
-            'Content-Type': 'application/json',
-            'auth': authToken
+    await axios.put(`${BASE_URL}/address`, payload,
+        { header: {
+            headers:{
+                'Content-Type': 'application/json',
+                'auth': authToken
+            }
         }
     })
     .then( response => {
@@ -15,4 +17,22 @@ export const address = async (payload, authToken) => {
         console.log("address", data)
         return { data }
     })
+}
+
+
+export const getRestaurant = async (authToken) => {
+    await axios.get(`${BASE_URL}/restaurants`,
+        { headers: {
+            'Content-Type': 'application/json',
+            auth: authToken
+        }})
+        .then( response => {
+            let data = {
+                status: response.status,
+                data: response.data.restaurants
+            }
+
+            console.log("get res cat",data)
+            return data;
+        })
 }
--- src/constant/urls.js
+++ src/constant/urls.js
@@ -1,2 +1,2 @@
 export const APP_NAME = 'fourFoodA'
-export const BASE_URL = `https://us-central1-missao-newton.cloudfunctions.net/${APP_NAME}/signup`
+export const BASE_URL = `https://us-central1-missao-newton.cloudfunctions.net/${APP_NAME}`
